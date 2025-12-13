ff09a2996d41ffb156b0321a453b1b5c43f77472
Darby Proctor <darby.proctor@gmail.com>
2025-12-13 16:26:28 -0500

    corrected version number

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -23,7 +23,7 @@ from src.api.browser_auth import SimpleBrowserAuthDialog, TokenBasedCanvasClient
 import keyring
 import requests
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 def check_for_updates():
     try:
